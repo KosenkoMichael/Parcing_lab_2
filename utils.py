e5f2c2bc6aec5d002ab6a313fed9d79925779dbc
KosenkoMichael <kosenko_michael_samara@mail.ru>
2023-10-14 19:13:22 +0400

    N_files_by_years

--- utils.py
+++ utils.py
@@ -19,4 +19,29 @@ def file_cut_date_and_data():
     print("end_1")
 
 
+def N_cut_by_year():
+    for year in range(2008, 2024):
+        data = []
+        with open("dataset.csv", "r", encoding="utf-8", newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if f".{year}" in row[0]:
+                    data.append(row)
+        time.sleep(0.1)
+        with open(f"2\\{year}0101-{year}1231.csv", "w", encoding="utf-8", newline="") as file_N:
+            writer = csv.writer(file_N)
+            writer.writerows(data)
+        time.sleep(0.1)
+    print("end_2")
+    # with open(f"{year}0101-{year+1}1231.csv", "w", encoding="utf-8", newline="") as file_N:
+    # writer = csv.writer(file_N)
+    # writer.writerows(row)
+
+
+def N_cut_by_week():
+    print('...')
+
+
 file_cut_date_and_data()
+N_cut_by_year()
+N_cut_by_week()
