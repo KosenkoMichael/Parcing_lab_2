045854ca85a3903cd33b8204e4b2e04e13d278f1
KosenkoMichael <kosenko_michael_samara@mail.ru>
2023-10-14 22:03:48 +0400

    N_files_by_weaks

--- utils.py
+++ utils.py
@@ -8,8 +8,8 @@ def file_cut_date_and_data():
     with open("dataset.csv", "r", encoding="utf-8", newline="") as file:
         reader = csv.reader(file)
         for row in reader:
-            date.append([row[0]])
-            data.append([row[i] for i in range(1, 7)])
+            date.append([row[i] for i in range(0, 3)])
+            data.append([row[i] for i in range(3, 9)])
     with open("1\\X.csv", "w", encoding="utf-8", newline="") as file_x:
         writer = csv.writer(file_x)
         writer.writerows(date)
@@ -25,7 +25,7 @@ def N_cut_by_year():
         with open("dataset.csv", "r", encoding="utf-8", newline="") as file:
             reader = csv.reader(file)
             for row in reader:
-                if f".{year}" in row[0]:
+                if row[2] == f"{year}":
                     data.append(row)
         time.sleep(0.1)
         with open(f"2\\{year}0101-{year}1231.csv", "w", encoding="utf-8", newline="") as file_N:
@@ -33,13 +33,28 @@ def N_cut_by_year():
             writer.writerows(data)
         time.sleep(0.1)
     print("end_2")
-    # with open(f"{year}0101-{year+1}1231.csv", "w", encoding="utf-8", newline="") as file_N:
-    # writer = csv.writer(file_N)
-    # writer.writerows(row)
 
 
 def N_cut_by_week():
-    print('...')
+    day_x = ""
+    for year in range(2008, 2024):
+        for month in range(1, 12):
+            for i in range(0, 5):
+                with open("dataset.csv", "r", encoding="utf-8", newline="") as file:
+                    reader = csv.reader(file)
+                    data = []
+                    for row in reader:
+                        for day in range(1+7*i, 8+7*i):
+                            if row[0] == f"{day}" and row[1] == f"{month}" and row[2] == f"{year}":
+                                data.append(row)
+                                day_x = row[0]
+                time.sleep(0.1)
+                if len(data):
+                    with open(f"3\\{year}.{month}.{1+7*i}-{year}.{month}.{day_x}.csv", "w", encoding="utf-8", newline="") as file_N:
+                        writer = csv.writer(file_N)
+                        writer.writerows(data)
+                time.sleep(0.1)
+    print("end_3")
 
 
 file_cut_date_and_data()
